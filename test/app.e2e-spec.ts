609a43bed2dc2f0bdccdb97409dde97d98367cfd
marshot <124557537+marshot9420@users.noreply.github.com>
2024-01-06 23:12:26 +0900

    test: 회원가입 API에 대한 E2E 테스트 추가

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,10 +16,36 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/auth/join (POST)', () => {
+    const newUser = {
+      email: `test${Date.now()}@example.com`,
+      password: 'Test1234',
+      username: 'TestUser',
+      name: '김테스터',
+    };
+
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/auth/join')
+      .send(newUser)
+      .expect(201);
+  });
+
+  it('/auth/join (POST) - Duplicate Email', async () => {
+    const newUser = {
+      email: `test${Date.now()}@example.com`,
+      password: 'Test1234',
+      username: 'TestUser',
+      name: '김테스터',
+    };
+
+    await request(app.getHttpServer())
+      .post('/auth/join')
+      .send(newUser)
+      .expect(201);
+
+    return request(app.getHttpServer())
+      .post('/auth/join')
+      .send(newUser)
+      .expect(400);
   });
 });
